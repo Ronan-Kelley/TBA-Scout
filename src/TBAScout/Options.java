1b2383c44310c30f8484b3f5c6042c3e58a9ea7a
Ronan <ronankelley2@gmail.com>
2018-11-28 09:18:01 -0500

    added support for flag -key
    TBA auth key can now be set via a launch argument

--- src/TBAScout/Options.java
+++ src/TBAScout/Options.java
@@ -4,7 +4,8 @@ public class Options {
     /**
      * this class exists entirely to keep track of command line options
      */
-    private Boolean cli = true;
+    private Boolean cli = false;
+    private String TBAKey = ""; //put your key here, or pass it to the program via command line
 
     //
     // getters and setters
@@ -16,5 +17,13 @@ public class Options {
 
     public Boolean getCli() {
         return cli;
+    }
+
+    public void setTBAKey(String TBAKey) {
+        this.TBAKey = TBAKey;
+    }
+
+    public String getTBAKey() {
+        return TBAKey;
     }
 }
--- src/TBAScout/TBAScout.java
+++ src/TBAScout/TBAScout.java
@@ -7,20 +7,45 @@ public class TBAScout {
 
     public static void main(String[] args) {
         /**
-         * command line options should mostly be handled here, and should
-         * follow the convention of starting with a dash
+         * command line options should mostly be handled here, and should follow the
+         * convention of starting with a dash
          */
-        for (String arg : args) {
-            if (args != null) {
+
+        System.out.println("launched");
+
+        if (args != null) {
+            for (String arg : args) {
                 switch (arg) {
-                    case "-cli":
-                        options.setCli(true);
-                        System.out.println("found argument: " + arg);
-                        break;
+                case "-cli":
+                    options.setCli(true);
+                    System.out.println("found argument: " + arg);
+                    break;
+                case "-key":
+                    Boolean foundDashKey = false;
+                    Boolean foundKey = false;
+                    String arg1 = "";
+                    for (int i = 0; i < args.length; i++) { //loop back through the array of arguments
+                        if (!foundDashKey) {
+                            if (args[i].equals("-key")) { //find the argument dictating a key
+                                foundDashKey = true;
+                            }
+                        } else if (foundDashKey && !foundKey) {
+                            if (args[i].contains("-") == false) {
+                                foundKey = false; //once the argument dictating a key is found, everything after it that comes before the next key should be the actual key
+                                arg1 += args[i];
+                            } else if (args[i].contains("-")) {
+                                foundKey = true;
+                            }
+                        }
+                        System.out.println(args[i]);
+                    }
+                    options.setTBAKey(arg1);
+                    System.out.println("found argument pair: " + arg + ": " + arg1);
+                    break;
                 }
-            } else if (args == null) {
-                System.out.println("no arguments found!");
             }
+        } else if (args == null) {
+            System.out.println("no arguments found!");
         }
 
         initUI();
@@ -28,19 +53,19 @@ public class TBAScout {
 
     private static void initUI() {
         /**
-         * not to be confused with the initUI found in Application.java,
-         * this method exists exclusively to put the graphical end of things
-         * in the event queue.
+         * not to be confused with the initUI found in Application.java, this method
+         * exists exclusively to put the graphical end of things in the event queue.
          */
         if (options.getCli() == false) {
             System.out.println("program launched");
-            
-		    EventQueue.invokeLater(() -> {
-			    Application exe = new Application();
-			    // uncomment to start maximized
-			    // exe.setExtendedState(JFrame.MAXIMIZED_BOTH);
-			    exe.setVisible(true);
-		    });
+
+            EventQueue.invokeLater(() -> {
+                Application exe = new Application();
+                // uncomment to start maximized
+                // exe.setExtendedState(JFrame.MAXIMIZED_BOTH);
+                exe.setVisible(true);
+                exe.pack();
+            });
         }
     }
 }
