db01b8bf2eff448256bc08971ef67202725743b1
Ronan Kelley <ronankelley2@gmail.com>
2018-12-12 19:46:47 -0500

    added pack() back into the gui init

--- src/TBAScout/TBAScout.java
+++ src/TBAScout/TBAScout.java
@@ -29,6 +29,7 @@ public class TBAScout {
                 // exe.setExtendedState(JFrame.MAXIMIZED_BOTH);
                 exe.setVisible(true);
                 exe.setBounds(0, 0, 500, 500);
+                exe.pack();
             });
         }
     }
