757141f6b0a9197f26ed9872cc788984bbf45215
Ronan Kelley <ronankelley2@gmail.com>
2018-12-12 20:04:04 -0500

    main window now auto-packs every 100 ms

--- src/TBAScout/MainWindow.java
+++ src/TBAScout/MainWindow.java
@@ -1,5 +1,6 @@
 package TBAScout;
 
+import java.awt.EventQueue;
 import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -11,40 +12,47 @@ import javax.swing.Timer;
 
 @SuppressWarnings("serial")
 public class MainWindow extends JPanel implements ActionListener {
-    //
-    // swing objects
-    //
+	//
+	// swing objects
+	//
 
-    private InputWindow inputWindow = new InputWindow(); 
-    private OutputWindow outputWindow = new OutputWindow();
-    private Timer timer = new Timer(100, this);
+	private InputWindow inputWindow = new InputWindow();
+	private OutputWindow outputWindow = new OutputWindow();
+	private GraphWindow graphWindow = new GraphWindow();
+	private Timer timer = new Timer(100, this);
 
-    public MainWindow() {
-        super(new GridLayout(1, 1));
-        
-        JTabbedPane tabbedPane = new JTabbedPane();
-        
-        tabbedPane.addTab("GET request settings", null, inputWindow,
-                "set GET request properties");
-        tabbedPane.setMnemonicAt(0, KeyEvent.VK_1);
-        
-        tabbedPane.addTab("GET request output", null, outputWindow,
-                "see GET request returned values");
-        tabbedPane.setMnemonicAt(1, KeyEvent.VK_2);
-        
-        //Add the tabbed pane to this panel.
-        add(tabbedPane);
-        
-        //The following line enables to use scrolling tabs.
-        tabbedPane.setTabLayoutPolicy(JTabbedPane.SCROLL_TAB_LAYOUT);
+	public MainWindow() {
+		super(new GridLayout(1, 1));
 
-        timer.start();
-    }
+		JTabbedPane tabbedPane = new JTabbedPane();
 
-    @Override
-    public void actionPerformed(ActionEvent e) {
-        if (e.getSource() == timer && inputWindow.getNewRequest()) {
-            outputWindow.setOutput(inputWindow.getRequestOutput());
-        }
+		tabbedPane.addTab("GET request settings", null, inputWindow, "set GET request properties");
+		tabbedPane.setMnemonicAt(0, KeyEvent.VK_1);
+
+		tabbedPane.addTab("GET request output", null, outputWindow, "see GET request returned values");
+		tabbedPane.setMnemonicAt(1, KeyEvent.VK_2);
+
+		tabbedPane.addTab("GET request output (graphs)", null, graphWindow,
+				"see GET request returned values in the form of a graph (where applicable)");
+		tabbedPane.setMnemonicAt(2, KeyEvent.VK_3);
+
+		// Add the tabbed pane to this panel.
+		add(tabbedPane);
+
+		// The following line enables to use scrolling tabs.
+		tabbedPane.setTabLayoutPolicy(JTabbedPane.SCROLL_TAB_LAYOUT);
+
+		timer.start();
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		if (e.getSource() == timer && inputWindow.getNewRequest()) {
+			outputWindow.setOutput(inputWindow.getRequestOutput());
+		}
+
+		EventQueue.invokeLater(() -> {
+			this.updateUI();
+		});
 	}
 }
