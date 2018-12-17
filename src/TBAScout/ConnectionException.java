9d5b8e71279b04a19db2acc1fcb29260a79eca78
Ronan Kelley <ronankelley2@gmail.com>
2018-12-17 18:52:36 -0500

    create ConnectionException.java

---
+++ src/TBAScout/ConnectionException.java
@@ -0,0 +1,24 @@
+package TBAScout;
+
+public class ConnectionException extends Exception {
+
+    public ConnectionException() {
+        // TODO Auto-generated constructor stub
+    }
+
+    public ConnectionException(String message) {
+        super(message);
+        // TODO Auto-generated constructor stub
+    }
+
+    public ConnectionException(Throwable cause) {
+        super(cause);
+        // TODO Auto-generated constructor stub
+    }
+
+    public ConnectionException(String message, Throwable cause) {
+        super(message, cause);
+        // TODO Auto-generated constructor stub
+    }
+    
+}
